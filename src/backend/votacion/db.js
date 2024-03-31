da70224b5e76e90be9cc7c323573b2711981f1c8
Agevega <agevega@hotmail.com>
2024-03-31 22:10:45 +0200

    Optimizacion de codigo de index.js para votacion tes 002

--- src/backend/votacion/db.js
+++ src/backend/votacion/db.js
@@ -5,6 +5,7 @@ const path = require('path');
 // Credenciales y configuración de conexión
 const username = encodeURIComponent('test');
 const password = encodeURIComponent('test2323test');
+const clusterEndpoint = 'matrix-instance-1.cl2iww8c0ioh.eu-west-1.docdb.amazonaws.com';
 const dbName = 'votacion_gatos_perros';
 
 // Opciones de conexión a MongoDB
